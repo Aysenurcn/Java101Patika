18f4bdc239a084d1327cde337504d6d1821cbc26
Ayşenur Canlıdır <45667467+Aysenurcn@users.noreply.github.com>
2023-09-24 09:10:25 +0300

    Add files via upload

---
+++ Mayin_Tarlasi/src/Main.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Oyun mineSwepper = new Oyun(4,4);
+        Oyun.run();
+    }
+}
